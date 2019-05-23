39e8d78dbe3fa6b3c6d9f6d94b1a5e421ab3dbf2
Jens Hviid <jens@hviid.ch>
2019-05-24 00:00:16 +0200

    Fix ship build

--- src/views/Shipyard.vue
+++ src/views/Shipyard.vue
@@ -219,7 +219,7 @@ export default {
       SteemConnectService.buildShip(
         this.loginUser,
         this.planetId,
-        ship.name,
+        ship.type,
         (error, result) => {
           if (error === null && result.success) {
             this.chainResponse.push(ship.longname);
