502c95cc52e2b9d70311824775c990209ecc7cab
Jens Hviid <jens@hviid.ch>
2019-06-02 10:55:27 +0200

    Add some more ranking figures

--- src/views/Ranking.vue
+++ src/views/Ranking.vue
@@ -3,19 +3,33 @@
     <h1>{{ $t("Ranking") }}</h1>
     <table>
       <thead>
-        <th>{{ $t("Rank") }}</th>
-        <th @click="sort('user')">{{ $t("User") }}</th>
-        <th @click="sort('meta_rate')">{{ $t("Production") }}</th>
-        <th @click="sort('planets')">{{ $t("Planets") }}</th>
-        <th @click="sort('explorations')">{{ $t("Explorations") }}</th>
+        <th><chevron-triple-up-icon :title="$t('Rank')" /></th>
+        <th @click="sort('user')"><account-icon :title="$t('User')" /></th>
+        <th @click="sort('meta_rate')">
+          <alpha-u-box-icon :title="$t('Production')" />
+        </th>
+        <th @click="sort('meta_skill')">
+          <school-icon :title="$t('Skill')" />
+        </th>
+        <th @click="sort('planets')"><earth-icon :title="$t('Planets')" /></th>
+        <th @click="sort('explorations')">
+          <magnify-icon :title="$t('Explorations')" />
+        </th>
+        <th @click="sort('ships')"><rocket-icon :title="$t('Ships')" /></th>
+        <th @click="sort('destroyed_ships_uranium')">
+          <nuke-icon :title="$t('Destroyed')" />
+        </th>
       </thead>
       <tbody>
         <tr v-for="(rank, index) in sortedRanking" :key="rank.user">
           <td>{{ index + 1 }}</td>
           <td>{{ rank.user }}</td>
           <td>{{ rank.meta_rate.toFixed(0) }}</td>
+          <td>{{ rank.meta_skill.toFixed(0) }}</td>
           <td>{{ rank.planets }}</td>
           <td>{{ rank.explorations }}</td>
+          <td>{{ rank.ships }}</td>
+          <td>{{ rank.destroyed_ships_uranium.toFixed(0) }}</td>
         </tr>
       </tbody>
     </table>
@@ -24,9 +38,27 @@
 
 <script>
 import RankingService from "@/services/ranking";
+import AlphaUBoxIcon from "vue-material-design-icons/AlphaUBox.vue";
+import ChevronTripleUpIcon from "vue-material-design-icons/ChevronTripleUp.vue";
+import AccountIcon from "vue-material-design-icons/Account.vue";
+import EarthIcon from "vue-material-design-icons/Earth.vue";
+import MagnifyIcon from "vue-material-design-icons/Magnify.vue";
+import RocketIcon from "vue-material-design-icons/Rocket.vue";
+import SchoolIcon from "vue-material-design-icons/School.vue";
+import NukeIcon from "vue-material-design-icons/Nuke.vue";
 
 export default {
   name: "ranking",
+  components: {
+    AlphaUBoxIcon,
+    ChevronTripleUpIcon,
+    AccountIcon,
+    EarthIcon,
+    MagnifyIcon,
+    RocketIcon,
+    SchoolIcon,
+    NukeIcon
+  },
   data: function() {
     return {
       ranking: null,
