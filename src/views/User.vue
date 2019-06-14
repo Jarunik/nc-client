8d0f6a7e5b165873b223c255341b57dc09ac4f44
Jens Hviid <jens@hviid.ch>
2019-06-14 20:07:06 +0200

    Revert "No loading for users"
    This reverts commit 69490941e640175a69b9fc612a71482c822c4238.

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="user">
+    <div v-if="user !== null">
       <h1>{{ $t("User") }}</h1>
       <p>{{ $t("Current user") }}: {{ gameUser }}</p>
       <p>
@@ -38,6 +39,9 @@
         </select>
       </p>
     </div>
+    <div v-else>
+      <Loading />
+    </div>
   </div>
 </template>
 
@@ -46,9 +50,13 @@ import UserService from "@/services/user";
 import PlanetsService from "@/services/planets";
 import moment from "moment";
 import { mapState } from "vuex";
+import Loading from "@/components/Loading.vue";
 
 export default {
   name: "user",
+  components: {
+    Loading
+  },
   data: function() {
     return {
       user: null,
