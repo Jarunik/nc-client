8bf11e8a7efebe6a496a779a3c77c298de5f483c
Jens Hviid <jens@hviid.ch>
2019-09-17 21:44:48 +0200

    Display up to 1000 planets

--- src/services/planets.js
+++ src/services/planets.js
@@ -2,13 +2,13 @@ import ApiService from "@/services/api";
 
 class PlanetsService {
   async byUser(user) {
-    const response = await ApiService.get(`/loadplanets?user=${user}`);
+    const response = await ApiService.get(`/loadplanets?user=${user}&to=1000`);
 
     return response;
   }
 
   async starterPlanet(user) {
-    const response = await ApiService.get(`/loadplanets?user=${user}`);
+    const response = await ApiService.get(`/loadplanets?user=${user}&to=1000`);
 
     const starter = response.planets.filter(planet => {
       return planet.starter === 1;
