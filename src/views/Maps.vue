214e2f6caf55b23af842730cc509cd463fee35ff
Jens Hviid <jens@hviid.ch>
2019-10-20 22:52:42 +0200

    Move and Zoom is ready

--- src/views/Maps.vue
+++ src/views/Maps.vue
@@ -10,10 +10,22 @@
         ref="canvas"
         :width="size"
         :height="size"
+        @click="focusCoordinate($event)"
+        v-on:dblclick="centerCoordinate($event)"
         style="width: 800px; height: 800px;"
       ></canvas>
     </div>
-    <input v-model="size" @blur="draw()" />
+    <input :value="search" @blur="updateSearch($event)" placeholder="x/y" />
+    <button @click="centerSearch(search)" v-tooltip="$t('Center on Selection')">
+      <target-variant-icon :title="$t('Focus')" />
+    </button>
+    <button @click="zoomOut()" v-tooltip="$t('Zoom Out')">
+      -
+    </button>
+    <button @click="zoomIn()" v-tooltip="$t('Zoom In')">
+      +
+    </button>
+    {{ size }}
   </div>
 </template>
 
@@ -21,9 +33,13 @@
 import MapService from "@/services/maps";
 import { mapState } from "vuex";
 import * as types from "@/store/mutation-types";
+import TargetVariantIcon from "vue-material-design-icons/TargetVariant.vue";
 
 export default {
   name: "maps",
+  components: {
+    TargetVariantIcon
+  },
   data: function() {
     return {
       planets: null,
@@ -33,12 +49,19 @@ export default {
       minY: 0,
       maxY: 0,
       height: 0,
+      canvas: null,
       ctx: null,
-      size: 1331
+      size: 1400,
+      focusX: 0,
+      focusY: 0,
+      centerX: 0,
+      centerY: 0,
+      search: null
     };
   },
   async mounted() {
-    this.ctx = this.$refs.canvas.getContext("2d");
+    this.canvas = this.$refs.canvas;
+    this.ctx = this.canvas.getContext("2d");
     await this.prepareComponent();
   },
   computed: {
@@ -49,21 +72,7 @@ export default {
       planetId: state => state.planet.id,
       planetName: state => state.planet.name,
       gameLocale: state => state.game.gameLocale
-    }),
-    xAxis() {
-      let xAxis = [];
-      for (var x = this.minX; x <= this.maxX; x++) {
-        xAxis.push(x);
-      }
-      return xAxis;
-    },
-    yAxis() {
-      let yAxis = [];
-      for (var y = this.minY; y <= this.minY + 50; y++) {
-        yAxis.push(y);
-      }
-      return yAxis;
-    }
+    })
   },
   methods: {
     async prepareComponent() {
@@ -74,7 +83,6 @@ export default {
             this.prepareComponent();
         }
       });
-      this.draw();
     },
     async getMap() {
       const response = await MapService.all();
@@ -104,18 +112,69 @@ export default {
         this.minY = minY;
         this.maxY = maxY;
         this.height = Math.abs(minY) + Math.abs(maxY) + 2;
+        this.draw();
       }
     },
     draw() {
-      this.planets.forEach(planet => {
-        this.ctx.fillStyle = "#FFFFFF";
-        this.ctx.fillRect(
-          planet.x + this.size / 2,
-          -planet.y + this.size / 2,
-          1,
-          1
-        );
+      this.$nextTick(() => {
+        this.clearCanvas();
+        if (this.planets !== null) {
+          this.planets.forEach(planet => {
+            let x = -this.centerX + this.size / 2 + planet.x;
+            let y = this.centerY + this.size / 2 - planet.y;
+            let planetSize = (1 / 800) * (this.size / 2);
+            if (planet.user == this.gameUser) {
+              this.ctx.fillStyle = "#008000"; //green
+              this.ctx.beginPath();
+              this.ctx.arc(x, y, 3 * planetSize, 0, 2 * Math.PI);
+              this.ctx.fillRect(x, y, 1, 1);
+              this.ctx.fill();
+            } else {
+              this.ctx.fillStyle = "#FFFFFF"; //white
+              this.ctx.beginPath();
+              this.ctx.arc(x, y, planetSize, 0, 2 * Math.PI);
+              this.ctx.fillRect(x, y, 1, 1);
+              this.ctx.fill();
+            }
+          });
+        }
       });
+    },
+    clearCanvas() {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    zoomIn() {
+      this.size = this.size - 200;
+      if (this.size <= 0) {
+        this.size = 100;
+      }
+      this.draw();
+    },
+    zoomOut() {
+      this.size = this.size + 200;
+      this.draw();
+    },
+    updateSearch(e) {
+      this.search = e.target.value;
+    },
+    centerSearch(search) {
+      let split = search.replace(/\s+/g, "").split("/");
+      this.centerX = parseInt(split[0]);
+      this.centerY = parseInt(split[1]);
+      this.draw();
+    },
+    focusCoordinate(event) {
+      let rect = this.canvas.getBoundingClientRect();
+      this.focusX =
+        this.centerX +
+        (event.clientX - rect.left - 800 / 2) * (this.size / 800);
+      this.focusY =
+        this.centerY + (rect.top - event.clientY + 800 / 2) * (this.size / 800);
+      this.search = parseInt(this.focusX) + "/" + parseInt(this.focusY);
+    },
+    centerCoordinate(event) {
+      this.focusCoordinate(event);
+      this.centerSearch(this.search);
     }
   }
 };
