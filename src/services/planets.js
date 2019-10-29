12328cefc3310db8d80b3278c30f4bf9c72a4df7
Jens Hviid <jens@hviid.ch>
2019-10-29 19:28:23 +0100

    don't load planets for null

--- src/services/planets.js
+++ src/services/planets.js
@@ -2,19 +2,31 @@ import ApiService from "@/services/api";
 
 class PlanetsService {
   async byUser(user) {
-    const response = await ApiService.get(`/loadplanets?user=${user}&to=1000`);
+    if (user !== null) {
+      const response = await ApiService.get(
+        `/loadplanets?user=${user}&to=1000`
+      );
 
-    return response;
+      return response;
+    } else {
+      return null;
+    }
   }
 
   async starterPlanet(user) {
-    const response = await ApiService.get(`/loadplanets?user=${user}&to=1000`);
+    if (user !== null) {
+      const response = await ApiService.get(
+        `/loadplanets?user=${user}&to=1000`
+      );
 
-    const starter = response.planets.filter(planet => {
-      return planet.starter === 1;
-    });
+      const starter = response.planets.filter(planet => {
+        return planet.starter === 1;
+      });
 
-    return starter[0];
+      return starter[0];
+    } else {
+      return null;
+    }
   }
 
   async byId(planetId) {
