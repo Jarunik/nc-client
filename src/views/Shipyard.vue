74dd2f1d3c53199b3b7256559ed3cc3a2b6e4c6f
Jens Hviid <jens@hviid.ch>
2019-11-24 08:12:38 +0100

    fix speed

--- src/views/Shipyard.vue
+++ src/views/Shipyard.vue
@@ -208,7 +208,10 @@
               }}</font>
             </td>
             <td>
-              {{ ship.speed }} ({{ ship.basespeed }}/{{ ship.battlespeed }})
+              <span v-if="ship.basespeed != null">
+                {{ ship.speed }} ({{ ship.basespeed }}/{{ ship.battlespeed }})
+              </span>
+              <span v-else>{{ ship.speed }}</span>
             </td>
             <td>
               {{ ship.costs.time | timePretty }}
